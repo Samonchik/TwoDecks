755d482fb198e9ce7f1c0ea98a57813cc631c4c1
Siamen Kadtsyn <kadcino@gmail.com>
2022-11-24 18:40:53 +0300

    Добавьте файлы проекта.

---
+++ TwoDecks/Deck.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows.Documents;
+
+namespace TwoDecks
+{
+    internal class Deck : ObservableCollection<Card>
+    {
+        private static Random random = new Random();
+
+        public Deck()
+        {
+            Reset();
+        }
+        public void Reset()
+        {
+            Clear();
+            for (int numberOfSuit = 0; numberOfSuit <= (int)Suits.Spides; numberOfSuit++)
+            {
+                for (int numberOfValue = 1; numberOfValue <= (int)Values.King; numberOfValue++)
+                {
+                    Add(new Card((Values)numberOfValue,(Suits)numberOfSuit));
+                }
+            }
+        }
+        public Card Deal(int index)
+        {
+            Card cardToDeal = base[index];
+            RemoveAt(index);
+            return cardToDeal;
+        }
+      
+        public void Shuffle()
+        {
+            var copyOfDeck = new List<Card>(this);
+            Clear();
+            while (copyOfDeck.Count > 0)
+            {
+                int index = random.Next(copyOfDeck.Count);
+                var randomCard = copyOfDeck[index];
+                Add(randomCard);
+                copyOfDeck.RemoveAt(index);
+            }
+        }
+        public void Sort()
+        {
+            List<Card> sortedCard = new List<Card>(this);
+            sortedCard.Sort(new CardComparerByValue());
+            Clear();
+            foreach (var card in sortedCard)
+            {
+                Add(card);
+            }
+        }
+    }
+}
